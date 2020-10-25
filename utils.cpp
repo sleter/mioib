2399a0f7a9163562ce0dc9da76fd4e1d757cee34
Bartosz Mikulski <bartmiki97@gmail.com>
2020-10-25 19:17:53 +0100

    Added .tsp file parsing for EUC_2D representation

--- utils.cpp
+++ utils.cpp
@@ -1,63 +1,148 @@
-#include<iostream>
-#include<vector>
-#include<numeric>
-#include<algorithm>
-#include<random>
-#include<chrono>
-#include<functional>
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <functional>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <memory>
 
 std::random_device rd;
 std::mt19937 gen(rd());
 
-size_t random_index(size_t from, size_t to){
-    std::uniform_int_distribution<size_t> distribution(from,  to);
+size_t random_index(size_t from, size_t to)
+{
+    std::uniform_int_distribution<size_t> distribution(from, to);
     return distribution(gen);
 }
 
-void print(std::vector<int> vec){
-    for(auto i : vec){ std::cout << i << " "; }
+struct coords
+{
+    int x, y;
+
+    coords(int x, int y) : x(x), y(y){}
+
+    friend std::ostream &operator<<(std::ostream &ostream, const coords &c)
+    {
+        ostream << "<" << c.x << "," << c.y << ">";
+        return ostream;
+    }
+};
+
+// Uses return value optimization (RVO), no copy
+std::vector<coords> parse_file(const std::string &path)
+{
+    auto vec = std::vector<coords>();
+
+    std::ifstream file;
+    std::string line;
+
+    file.open(path);
+
+    if (file.is_open())
+    {
+        int nodeId;
+        float x, y;
+
+        std::regex re("^(\\w+)\\W*:\\W*(\\w+)$");
+        std::smatch matches;
+
+        while (std::getline(file, line))
+        {
+            // Reserve needed memory
+            if (std::regex_match(line, matches, re) && matches[1].compare("DIMENSION") == 0)
+                vec.reserve(std::stoi(matches[2]));
+
+            else if (line.compare("NODE_COORD_SECTION") == 0)
+                break;
+        }
+
+        while (file >> nodeId >> x >> y)
+        {
+            // Construct objects in correct memory address, no copy
+            vec.emplace_back(static_cast<int>(x), static_cast<int>(y));
+            if (line.compare("EOF") == 0)
+                break;
+        }
+
+        file.close();
+    }
+
+    return vec;
+}
+
+template <typename T>
+void print(const std::vector<T> &vec)
+{
+    for (auto &i : vec)
+    {
+        std::cout << i << " ";
+    }
     std::cout << std::endl;
 }
 
-void shuffle(std::vector<int>& v){
-    for(size_t i = v.size() - 1; i > 0; --i){
+// Shuffle vector with uniform distribution
+template <typename T>
+void shuffle(std::vector<T> &v)
+{
+    for (size_t i = v.size() - 1; i > 0; --i)
+    {
         std::swap(v[random_index(0, i)], v[i]);
     }
 }
 
-std::chrono::high_resolution_clock::time_point now(){
+std::chrono::high_resolution_clock::time_point now()
+{
     return std::chrono::high_resolution_clock::now();
 }
 
-int64_t as_milliseconds(std::chrono::nanoseconds time){
+int64_t as_milliseconds(std::chrono::nanoseconds time)
+{
     return std::chrono::duration_cast<std::chrono::milliseconds>(time).count();
 }
 
-int64_t measure_time(std::function<void()> f, long limit_ms){
+int64_t measure_time(std::function<void()> f, long limit_ms)
+{
     auto start_time = now();
     uint32_t iteration = 0;
     do
     {
         f();
         ++iteration;
-        // std::cout << as_milliseconds(now() - start_time) << std::endl;
     } while (as_milliseconds(now() - start_time) < limit_ms);
     return as_milliseconds(now() - start_time) / (int64_t)iteration;
 }
 
-void test_function(){
+void test_function()
+{
     std::vector<int> v(1000000);
     std::iota(v.begin(), v.end(), 0);
     shuffle(v);
 }
 
-int main(){
-    long total_time = 0;
-    int iters = 10;
-    for(int i=0; i<iters; i++){
-        total_time += measure_time(test_function, 100);
+int main(int argc, char *argv[])
+{
+    if (argc != 2){
+        std::cout << "Usage: " << argv[0] << " <file>.tsp" << '\n';
+        return 0; 
     }
-    // 5.048 ms for vector of 5000 and 500 iters 
-    std::cout << (double)total_time/iters << " ms" << std::endl;
+
+    std::string path(argv[1]);
+    auto v = parse_file(path);
+    
+    print(v);
+    // auto v = parse_file("/home/bartek/Repos/mioib/tsplib/EUC_2D/pr2392.tsp");
+    // print(v);
+
+    // long total_time = 0;
+    // int iters = 10;
+    // for(int i=0; i<iters; i++){
+    //     total_time += measure_time(test_function, 100);
+    // }
+    // // 5.048 ms for vector of 5000 and 500 iters
+    // std::cout << (double)total_time/iters << " ms" << std::endl;
     return 0;
 }
