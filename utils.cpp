f899f3be13bf23e445b880e3813cd90b5b63aa00
Bartosz Mikulski <bartmiki97@gmail.com>
2020-10-25 13:44:30 +0100

    Updated shuffle algorithm

--- utils.cpp
+++ utils.cpp
@@ -19,33 +19,11 @@ void print(std::vector<int> vec){
     std::cout << std::endl;
 }
 
-class Generator {
-    std::vector<int> vec;
-    int new_size = 0;
-
-public:
-    Generator(size_t n){
-        vec = std::vector<int>(n);
-        reset();
+void shuffle(std::vector<int>& v){
+    for(size_t i = v.size() - 1; i > 0; --i){
+        std::swap(v[random_index(0, i)], v[i]);
     }
-
-    void reset(){
-        std::iota(vec.begin(), vec.end(), 0);
-        new_size = vec.size() - 1;
-    }
-
-    bool has_next(){
-        return new_size >= 0;
-    }
-
-    std::vector<int>& next(){
-        if(new_size > 0 && new_size < vec.size()-1){
-            std::swap(vec[random_index(0, new_size-1)], vec[new_size]);
-        }
-        --new_size;
-        return vec;
-    }
-};
+}
 
 std::chrono::high_resolution_clock::time_point now(){
     return std::chrono::high_resolution_clock::now();
@@ -68,22 +46,14 @@ int64_t measure_time(std::function<void()> f, long limit_ms){
 }
 
 void test_function(){
-    Generator g(5000);
-    while (g.has_next()){
-        g.next();
-    }
+    std::vector<int> v(1000000);
+    std::iota(v.begin(), v.end(), 0);
+    shuffle(v);
 }
 
 int main(){
-
-    // auto g = Generator(5);
-    // while (g.has_next())
-    // {
-    //     print(g.next());
-    // }
-    
     long total_time = 0;
-    int iters = 100;
+    int iters = 10;
     for(int i=0; i<iters; i++){
         total_time += measure_time(test_function, 100);
     }
